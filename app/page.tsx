002309491c0e7cb07f9ed43d0274f93643c2eba9
priyansh-narang2308 <priyanshnarang23@gmail.com>
2026-09-13 10:43:23 +0530

    feat: add theme provider and animated theme toggle components using next-themes and motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { ThemeToggle } from "@/components/motion/theme-toggle";
+
 export default function Home() {
   return (
     <div>
       <h1>Promptwars</h1>
+      <ThemeToggle
+        variant={"rectangle"}
+        start="bottom-up"
+        className="rounded-xl cursor-pointer border border-border bg-background p-2.5"
+        iconClassName="h-5 w-5"
+      />
     </div>
   );
 }
